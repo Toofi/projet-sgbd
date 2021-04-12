521ca21aee6c72d15967fc77e63cf804e78ca01c
Herpoel Quentin <herpoel.quentin@gmail.com>
2021-04-12 23:45:06 +0200

    connexion enabled

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 const express = require('express');
 
+const databaseConnexion = require('./database/connexion');
+
 const app = express();
 const port = 3000;
 
+(async () => {
+  console.log('coucou');
+  const db = await databaseConnexion();
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
+})();
 
 
-setInterval(() => {
-  console.log("coucou");
-}, 1000);
+  // let count = 0;
+  // setInterval(() => {
+  //   console.log(`coucou ${count}`);
+  //   count += 1;
+  // }, 1000);
