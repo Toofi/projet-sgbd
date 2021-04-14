756306828f7afd6d3dadd8d7dbe67dc9c7ca6522
Herpoel Quentin <herpoel.quentin@gmail.com>
2021-04-14 11:35:47 +0200

    ajout des constraintes dans la connexion.js

--- database/connexion.js
+++ database/connexion.js
@@ -1,7 +1,10 @@
 const MongoClient = require('mongodb').MongoClient;
 const userConstraints = require('./usersConstraints');
+const pricesConstraints = require('./pricesConstraints');
+const productsConstraints = require('./productsConstraints');
+const alertsConstraints = require('./alertsConstraints');
 
-const url = "mongodb://localhost:29740";
+const url = "mongodb://localhost:29203";
 const dbName = 'quentin';
 
 const getDb = async () => {
@@ -10,6 +13,9 @@ const getDb = async () => {
     const client = await MongoClient.connect(url, { useUnifiedTopology: true });
     db = client.db(dbName);
     await userConstraints(db);
+    await pricesConstraints(db);
+    await productsConstraints(db);
+    await alertsConstraints(db);
   } catch (error) {
     console.error(error);
   }
