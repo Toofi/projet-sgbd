65242260260c01eba13e2f1e3ca69ac22293d6af
Herpoel Quentin <herpoel.quentin@gmail.com>
2021-05-03 00:30:01 +0200

    ajout de la création d'alertes pendant le scan

--- controllers/prices.js
+++ controllers/prices.js
@@ -1,5 +1,5 @@
 const { response } = require('express');
-const { Db, ObjectID } = require('mongodb');
+const { Db, ObjectID, Decimal128 } = require('mongodb');
 const Puppeteer = require('./puppeteer');
 
 let cron = require('node-cron');
@@ -10,10 +10,47 @@ module.exports = async (app, db) => {
   }
   const pricesCollection = db.collection("prices");
   const productsCollection = db.collection("products");
+  const alertsCollection = db.collection("alerts");
 
   let productsIdAndUrls = await productsCollection.aggregate([{
     $project: { id: 1, url: 1 }
   }]).toArray();
+
+  let toDecimal = (price) => {
+    if (price.includes('€')) {
+      return price.replace('€', '').trim().replace(',', '.');
+    }
+  }
+
+  let getLatestPrice = async (productId) => {
+    try {
+      const latestPrice = await pricesCollection.aggregate([
+        {
+          $match: {
+            productId: productId
+          }
+        }, {
+          $group: {
+            _id: null,
+            latestDocId: {
+              $max: '$_id'
+            },
+            latestPrice: {
+              $max: '$price'
+            }
+          }
+        }, {
+          $project: {
+            _id: 0,
+            latestPrice: 1
+          }
+        }
+      ]).toArray();
+      return latestPrice[0].latestPrice.toString();
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
   let puppet = new Puppeteer();
 
@@ -25,21 +62,41 @@ module.exports = async (app, db) => {
         let data = {
           productId: new ObjectID(products[e]._id),
           date: new Date(),
-          price: scrappedPrice.price,
+          price: toDecimal(scrappedPrice.price),
           isPromo: scrappedPrice.isPromo,
         };
-        const response = await pricesCollection.insertOne(data)
+        const response = await pricesCollection.insertOne({
+          productId: data.productId,
+          date: data.date,
+          price: Decimal128.fromString(data.price),
+          isPromo: data.isPromo
+        });
         if (response.result.n !== 1 || response.result.ok !== 1) {
           console.log("impossible to create the price");
         }
         const [price] = response.ops;
-        console.log('༼ つ ◕_◕ ༽つ '+e+': postPrices ok');
+        console.log('༼ つ ◕_◕ ༽つ ' + e + ': postPrices ok');
+        const latestPrice = await getLatestPrice(data.productId);
+        if (data.isPromo === true) {
+          console.log("Création d'une alerte car en promo ...");
+          const alert = await alertsCollection.insertOne({
+            productId: data.productId,
+            price: Decimal128.fromString(data.price)
+          });
+        } else if (data.price < latestPrice) {
+          console.log("Création d'une alerte car baisse de produit ...");
+          const alert = await alertsCollection.insertOne({
+            productId: data.productId,
+            price: Decimal128.fromString(data.price)
+          });
+        } else {}
+
       }
     } catch (e) {
       console.log(e);
     }
   };
-
+  //'1 * * * *'
   cron.schedule('1 * * * *', () => {
     console.log('scan de tous les produits toutes les heures ...');
     scanPrices(productsIdAndUrls);
@@ -49,5 +106,38 @@ module.exports = async (app, db) => {
     let prices = await pricesCollection.find().toArray();
     res.json(prices);
   });
+
+  app.get('/api/latestPrice', async (req, res) => {
+    try {
+
+      let latestPrice = await pricesCollection.aggregate([
+        {
+          $match: {
+            productId: new ObjectID("60884855a4a9284e7cdec24e")
+          }
+        }, {
+          $group: {
+            _id: null,
+            latestDocId: {
+              $max: '$_id'
+            },
+            latestPrice: {
+              $max: '$price'
+            }
+          }
+        }, {
+          $project: {
+            _id: 0,
+            latestPrice: 1
+          }
+        }
+      ]).toArray();
+      console.log(latestPrice.latestPrice);
+      res.json(latestPrice)
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  );
 
 };
